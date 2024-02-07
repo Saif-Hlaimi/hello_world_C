f6150925559dc00e5aabc3dd6af92149bb1c22cb
Saif_Hlaimi <saif.hlaimi@gmail.com>
2024-02-07 15:45:40 +0100

    Initial commit

--- hellogithub.c
+++ hellogithub.c
@@ -2,5 +2,9 @@
 
 int main() {
    printf("Hello, World!");
+   printf(" we gonna print hello world 10 times ");
+   for (int i = 0 ; i < 10 ; i++){
+      printf("hello world !");
+   }
    return 0;
 }
